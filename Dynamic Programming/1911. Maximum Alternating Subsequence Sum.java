4886751fa87a5891f3f5fa63536579d3c3c7ae0b
Raj Sharma <128994172+rajsharma25er@users.noreply.github.com>
2025-04-20 10:58:43 +0530

    new

--- Dynamic Programming/1911. Maximum Alternating Subsequence Sum.java
+++ Dynamic Programming/1911. Maximum Alternating Subsequence Sum.java
@@ -32,3 +32,22 @@ class Solution {
 
     }
 }
+// Tabulation Methode 
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        int n =  nums.length;
+        long dp[][] = new long[n+1][2];
+
+        for(int i = 1; i<=n; i++){
+            dp[i][0]= Math.max((dp[i-1][1]- nums[i-1]),dp[i-1][0]);
+            dp[i][1]= Math.max((dp[i-1][0]+ nums[i-1]),dp[i-1][1]);
+
+        }
+        
+
+        return Math.max(dp[n][0],dp[n][1]);
+    }
+ }
+
+
+    
