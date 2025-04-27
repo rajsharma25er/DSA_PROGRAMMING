ecccbff3c17a57e8e9762137ed9dbd55f909c377
Raj Sharma <128994172+rajsharma25er@users.noreply.github.com>
2025-04-27 10:17:32 +0530

    new

--- Dynamic Programming/983. Minimum Cost For Tickets.java
+++ Dynamic Programming/983. Minimum Cost For Tickets.java
@@ -34,3 +34,35 @@ class Solution {
         return dp[i] = Math.min(cost1,Math.min(cost2,cost3));
     }
 }
+
+import java.util.Arrays;
+
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int n = days.length;
+        int[] dp = new int[n + 1];
+
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int cost1 = costs[0] + dp[i + 1];
+
+            int j = i;
+            while (j < n && days[j] < days[i] + 7) {
+                j++;
+            }
+            int cost2 = costs[1] + dp[j];
+
+            int k = i;
+            while (k < n && days[k] < days[i] + 30) {
+                k++;
+            }
+            int cost3 = costs[2] + dp[k];
+
+            dp[i] = Math.min(cost1, Math.min(cost2, cost3));
+        }
+
+        return dp[0];
+    }
+}
+
